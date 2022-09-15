26003eb41c9c39fd8eee3a30b1e5d90261d4d3cf
Ralph Metel <ralph.metel@exxeta.com>
2022-09-15 18:02:02 +0200

    changed table to "tasks", since task is preserved word

--- src/main/java/com/todo/todobackend/models/Task.java
+++ src/main/java/com/todo/todobackend/models/Task.java
@@ -2,15 +2,13 @@ package com.todo.todobackend.models;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
+@Table(name = "tasks")
 public class Task {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
