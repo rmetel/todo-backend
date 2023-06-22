e7b4aa1337a3bcb1f8080dcffebf246727fc7ef0
ralph.metel@gmx.de <pOison_2(>
2023-06-22 22:33:20 +0200

    added ssh key instructions

---
+++ ssh/ssh.sh
@@ -0,0 +1,10 @@
+# go to .ssh
+cd ~/.ssh
+
+# generate ssh key
+ssh-keygen -t ed25519 -C "ralph.metel@gmx.de"
+
+# copy public key to ~/.ssh/authorized_keys
+ssh-copy-id -i ~/.ssh/id_ed25519.pub root@212.227.201.99
+
+# add private key to the service!
