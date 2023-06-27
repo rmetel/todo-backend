a9abc9d658cfebb97f279d61ac1a4b96422e72dd
ralph.metel@gmx.de <pOison_2(>
2023-06-27 10:55:50 +0200

    added changes

--- ssh/ssh.sh
+++ ssh/ssh.sh
@@ -9,6 +9,9 @@ ssh-keygen -t ed25519 -C "ralph.metel@gmx.de"
 # copy public key to ~/.ssh/authorized_keys
 ssh-copy-id -i ~/.ssh/id_ed25519.pub root@212.227.201.99
 
+# add git to known hosts
+ssh-keyscan github.com >> C:\Users\ddr_r\.ssh\known_hosts
+
 # add private key to the service!
 
 # github -> execute this command on server to authenticate! delete repo and clone it wia ssh again!
