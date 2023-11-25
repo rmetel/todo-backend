149d20bd067efc06721dd052b6e5e15d2c040527
ralph.metel@gmx.de <pOison_2(>
2023-11-25 23:43:29 +0100

    pass build version to server

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-TODO_FRONTEND_VERSION=$(cat ~/logs/todo-frontend.log)
-TODO_BACKEND_VERSION=$(cat ~/logs/todo-backend.log)
+TODO_FRONTEND_VERSION=$(cat ~/logs/build/todo-frontend.log)
+#TODO_BACKEND_VERSION=$(cat ~/logs/build/todo-backend.log)
+TODO_BACKEND_VERSION=$(grep -oPm2 "(?<=<version>)[^<]+" ~/logs/build/todo-backend.log | sed -n 2p)
 
 export TODO_FRONTEND_VERSION
 export TODO_BACKEND_VERSION
