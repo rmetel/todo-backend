7fad88746b0e9ec8d0e54fdd1558234966eb67f2
ralph.metel@gmx.de <pOison_2(>
2023-11-26 02:08:28 +0100

    get build properties

--- src/main/java/com/todo/todobackend/TodoBackendApplication.java
+++ src/main/java/com/todo/todobackend/TodoBackendApplication.java
@@ -12,27 +12,27 @@ import org.springframework.core.io.ClassPathResource;
 
 @SpringBootApplication/*(exclude = {DataSourceAutoConfiguration.class})*/
 @OpenAPIDefinition(info = @Info(
-	title = "ToDo Api",
-	version = "1.0",
-	description = "API for task administration",
-	contact = @Contact(
-		name = "Ralph Metel",
-		email = "ralph.metel@exxeta.com"
-	)
+        title = "ToDo Api",
+        version = "1.0",
+        description = "API for task administration",
+        contact = @Contact(
+                name = "Ralph Metel",
+                email = "ralph.metel@exxeta.com"
+        )
 ))
 public class TodoBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TodoBackendApplication.class, args);
-	}
+    }
 
-	@Bean
-	public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
-		PropertySourcesPlaceholderConfigurer propsConfig
-				= new PropertySourcesPlaceholderConfigurer();
-		propsConfig.setLocation(new ClassPathResource("git.properties"));
-		propsConfig.setIgnoreResourceNotFound(true);
-		propsConfig.setIgnoreUnresolvablePlaceholders(true);
-		return propsConfig;
-	}
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+        PropertySourcesPlaceholderConfigurer propsConfig
+                = new PropertySourcesPlaceholderConfigurer();
+        propsConfig.setLocation(new ClassPathResource("git.properties"));
+        propsConfig.setIgnoreResourceNotFound(true);
+        propsConfig.setIgnoreUnresolvablePlaceholders(true);
+        return propsConfig;
+    }
 }
