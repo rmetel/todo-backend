24c92e34e491bc9e9decf599367780fe6f3827f1
ralph.metel@gmx.de <pOison_2(>
2023-11-26 01:00:11 +0100

    set branch variable

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,16 @@
 
 TODO_FRONTEND_VERSION=$(cat ~/logs/build/todo-frontend.log)
 TODO_BACKEND_VERSION=$(grep -oPm1 "(?<=<version>)[^<]+" ~/logs/build/todo-backend.log)
+TODO_BACKEND_BRANCH=$(grep -oPm1 "(?<=<branch>)[^<]+" ~/logs/build/todo-backend.log)
 
 export TODO_FRONTEND_VERSION
+
 export TODO_BACKEND_VERSION
+export TODO_BACKEND_BRANCH
 
 cd ~/git/todo-backend || exit
 
-git pull
+git switch "$TODO_BACKEND_BRANCH" && git pull
 
 docker-compose -f docker/docker-compose-prod.yaml up -d
 
