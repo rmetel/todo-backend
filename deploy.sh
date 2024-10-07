694b064d1304c8245334eff894d78fd60d3974ba
Ralph Metel <ralph.metel@gmx.de>
2024-10-07 20:19:11 +0200

    devision.tech (#11)
    * allow requests from devision.tech
    
    * fixed path
    
    * next try
    
    * next try
    
    * ...and again
    
    * sync docker compose
    
    * fixed workflow name
    
    * raised to 1.4.1
    
    * fixed log file name
    
    * added executor property
    
    * renamed workflow
    
    ---------
    
    Co-authored-by: ralph.metel@gmx.de <pOison_2(>

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
-TODO_FRONTEND_VERSION=$(cat ~/logs/todo-frontend.log)
 TODO_BACKEND_VERSION=$(grep -oPm1 "(?<=<version>)[^<]+" ~/logs/build/todo-backend.log)
 TODO_BACKEND_BRANCH=$(grep -oPm1 "(?<=<branch>)[^<]+" ~/logs/build/todo-backend.log)
-
-export TODO_FRONTEND_VERSION
 
 export TODO_BACKEND_VERSION
 export TODO_BACKEND_BRANCH
@@ -12,11 +9,11 @@ export TODO_BACKEND_BRANCH
 cd ~/git/todo-backend || exit
 
 # todo: switch branch
-#git switch "$TODO_BACKEND_BRANCH"
+git switch "$TODO_BACKEND_BRANCH"
 
 git pull
 
-docker-compose -f docker/docker-compose-prod.yaml up -d
+docker-compose -f docker/todo-app/docker-compose-prod.yaml up -d
 
 # retrieve version
 # docker inspect todo-backend | grep -o 'todo-backend:.*' | cut -f2 -d: | cut -f1 -d '"'
